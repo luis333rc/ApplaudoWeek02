b4d4e16403f38784c39fe5f19967de8b7b2fc64c
Luis <luis.w.rivera.c@gmail.com>
2023-02-21 12:30:09 -0500

    challenge 01 completed

---
+++ Challenge01/EntityGeneric.cs
@@ -0,0 +1,21 @@
+﻿namespace Challenge01
+{
+    public class EntityGeneric<TKey, TValue> : IEntity<TKey>
+    {
+        public TKey Id { get ; set; }
+
+        public TValue Value { get; set; }
+
+
+        public EntityGeneric(TKey tkey, TValue tvalue)
+        {
+            Id = tkey;
+            Value = tvalue;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Value: {Value}";
+        }
+    }
+}
