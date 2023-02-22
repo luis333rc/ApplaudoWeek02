40304bef2a691e8afe80987d432e1319bf1bbf3c
Luis <luis.w.rivera.c@gmail.com>
2023-02-21 19:03:06 -0500

    challenge 03 completed

---
+++ Challenge03/Program.cs
@@ -0,0 +1,41 @@
+﻿using Challenge02;
+using System.Text;
+
+namespace Challenge03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var val = new Validator<int>(num => {
+
+                var errors = new List<string>();
+
+                if (num < 10)
+                    return null;
+
+                errors.Add("It's equals or greater than 10");
+
+                if (num % 2 == 1)
+                    errors.Add("It's odd");
+
+                return errors;
+            });
+
+            Console.WriteLine(val.IsValid(15));
+            Console.WriteLine(val.HasErrors());
+
+            Console.WriteLine(val.GetErrors());
+        }
+    }
+
+    public static class ValidatorExtensions
+    {
+        public static string GetErrors<T>(this Validator<T> validator)
+        {
+            var errors = validator.Errors.ToArray();
+
+            return string.Join(", ", errors);
+        }
+    }
+}
